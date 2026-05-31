a68cac8b6840f9d2636575a665c61bbcf1563f65
Jules Hasan <juliemarlina@polipd.edu.my>
2026-05-31 17:14:58 +0800

    Update backend URL for SMART_MUET_BACKEND

--- js/backend-config.js
+++ js/backend-config.js
@@ -12,7 +12,7 @@
  */
 
 const SMART_MUET_BACKEND_ENABLED = false; // set true after deploying Apps Script
-const SMART_MUET_BACKEND_URL     = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const SMART_MUET_BACKEND_URL     = 'https://script.google.com/macros/s/AKfycbwDzyCsrFvVxNjvcwZxgEKRRi_VOdWKUxs_1Q0Rgess5z3h8rQS-WwmlNN1Pmh9lrM6lA/exec;'
 
 async function saveSmartMuetAttempt(payload) {
   // Always save to localStorage as the primary record
